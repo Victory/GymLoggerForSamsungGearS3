76d5018173ee6c4deab24566fca896dd4a1b2c9b
Victory <git@dfhu.org>
2017-04-08 19:00:51 -0500

    base js files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-
-window.onload = function () {
-    // TODO:: Do your initialization job
-
+(function() {
+  var bindDefaultKeys = function() {
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-            try {
-                tizen.application.getCurrentApplication().exit();
-            } catch (ignore) {
-            }
+      if (e.keyName == "back")
+        try {
+          tizen.application.getCurrentApplication().exit();
+        } catch (ignore) {
+        }
     });
+  };
 
-    // Sample code
-    var textbox = document.querySelector('.contents');
-    textbox.addEventListener("click", function(){
-        box = document.querySelector('#textbox');
-        box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
-    });
+  var init = function() {
+    bindDefaultKeys();
+    console.log('we init now');
+    console.log(vQuery);
+    console.log(vPromise);
+  };
 
-};
+  window.onload = init;
+})();
