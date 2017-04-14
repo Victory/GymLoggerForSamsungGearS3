fea6235799523677a99c249be937b87cf0f9e83b
Victory <git@dfhu.org>
2017-04-14 18:49:55 -0500

    use jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  var vq = window.vQuery;
+  var db;
 
   var bindDefaultKeys = function() {
     // add eventListener for tizenhwkey
@@ -13,14 +13,36 @@
   };
 
   var bindHomeClick = function() {
-    vq('#chooseExercise').on('click', function (evt) {
-      vq("#mainBox").load('choose-exercise.html');
+    $('#chooseExercise').on('click', function (evt) {
+      $("#mainBox").load('choose-exercise.html');
     });
+  };
+  
+  var bindExerciseOption = function() {
+    $('ul#exerciseOptions li').on('click', function(evt) {
+      console.log(this);
+    });
+  };
+
+  var initDb = function() {
+   
+    var version = '0.1';
+    var dbName = 'gymlogger';
+    var dbDisplayName = 'gymlogger_db'
+    var dbSize = 2 << 20; 
+    
+    var cb = function (database) {
+        console.log('created db');
+    };
+
+    db = openDatabase(dbName, version, dbDisplayName, dbSize, cb);
   };
 
   var init = function() {
     bindDefaultKeys();
+    bindExerciseOption();
     bindHomeClick();
+    initDb();
   };
 
   window.onload = init;
